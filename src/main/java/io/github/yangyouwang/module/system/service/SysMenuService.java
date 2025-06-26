6b8b0d9caaa6ecefe44c2e56cc7f9cf86a8fc257
xcmrfc-thanos <xcmrfc@foxmail.com>
2025-06-26 15:24:06 +0800

    修复 Vulnerable to XSS https://gitee.com/yangyouwang/crud/issues/IBSPOX

--- src/main/java/io/github/yangyouwang/module/system/service/SysMenuService.java
+++ src/main/java/io/github/yangyouwang/module/system/service/SysMenuService.java
@@ -3,15 +3,15 @@ package io.github.yangyouwang.module.system.service;
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.bean.copier.CopyOptions;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import io.github.yangyouwang.common.base.domain.TreeSelectNode;
 import io.github.yangyouwang.common.base.service.BaseService;
 import io.github.yangyouwang.common.constant.ConfigConsts;
-import io.github.yangyouwang.common.base.domain.TreeSelectNode;
+import io.github.yangyouwang.framework.util.StringUtil;
 import io.github.yangyouwang.framework.util.converter.ListToTree;
 import io.github.yangyouwang.framework.util.converter.impl.ListToTreeImpl;
 import io.github.yangyouwang.framework.web.exception.BusinessException;
-import io.github.yangyouwang.framework.util.StringUtil;
-import io.github.yangyouwang.module.system.mapper.SysMenuMapper;
 import io.github.yangyouwang.module.system.entity.SysMenu;
+import io.github.yangyouwang.module.system.mapper.SysMenuMapper;
 import io.github.yangyouwang.module.system.model.vo.SysMenuVO;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.security.access.AccessDeniedException;
@@ -48,7 +48,8 @@ public class SysMenuService extends BaseService<SysMenuMapper, SysMenu> {
 
     /**
      * 根据用户查询菜单
-     * @param userId 用户ID
+     *
+     * @param userId   用户ID
      * @param userName 用户名称
      * @return 菜单信息
      */
@@ -62,7 +63,7 @@ public class SysMenuService extends BaseService<SysMenuMapper, SysMenu> {
         }
         List<SysMenuVO> sysMenuVOList = menus.stream().map(sysMenu -> {
             SysMenuVO sysMenuVO = new SysMenuVO();
-            BeanUtil.copyProperties(sysMenu, sysMenuVO, true, CopyOptions.create().setIgnoreNullValue(true).setIgnoreError(true));
+            BeanUtil.copyProperties(sysMenu, sysMenuVO, CopyOptions.create().setIgnoreNullValue(true).setIgnoreError(true));
             return sysMenuVO;
         }).collect(Collectors.toList());
         ListToTree treeBuilder = new ListToTreeImpl();
@@ -71,9 +72,10 @@ public class SysMenuService extends BaseService<SysMenuMapper, SysMenu> {
 
     /**
      * 删除请求
+     *
      * @param id 菜单id
      */
-    @Transactional(isolation = Isolation.DEFAULT,propagation = Propagation.REQUIRED,rollbackFor = Throwable.class)
+    @Transactional(isolation = Isolation.DEFAULT, propagation = Propagation.REQUIRED, rollbackFor = Throwable.class)
     public void remove(Long id) {
         // 删除子菜单
         int count = this.count(new LambdaQueryWrapper<SysMenu>()
@@ -85,14 +87,16 @@ public class SysMenuService extends BaseService<SysMenuMapper, SysMenu> {
             sysRoleMenuService.removeSysRoleMenuByMenuId(id);
         }
     }
+
     /**
      * 查询菜单列表
+     *
      * @return 菜单列表
      */
     @Transactional(readOnly = true)
     public List<TreeSelectNode> treeSelect() {
         List<SysMenu> menus = this.list(new LambdaQueryWrapper<SysMenu>()
-                .eq(SysMenu::getVisible,ConfigConsts.SYS_YES));
+                .eq(SysMenu::getVisible, ConfigConsts.SYS_YES));
         if (menus.isEmpty()) {
             return Collections.emptyList();
         }
@@ -109,6 +113,7 @@ public class SysMenuService extends BaseService<SysMenuMapper, SysMenu> {
 
     /**
      * 根据菜单ids查询选中菜单树结构
+     *
      * @param ids 菜单ids
      * @return 菜单树结构
      */
@@ -117,7 +122,7 @@ public class SysMenuService extends BaseService<SysMenuMapper, SysMenu> {
         List<SysMenu> menus = this.list(new LambdaQueryWrapper<>());
         // 父菜单ID是否存在顶级
         boolean flag = menus.stream().anyMatch(s -> 0 == s.getParentId());
-        if (menus.isEmpty()|| !flag) {
+        if (menus.isEmpty() || !flag) {
             return Collections.emptyList();
         }
         List<TreeSelectNode> result = menus.stream().map(sysMenu -> {
@@ -126,7 +131,7 @@ public class SysMenuService extends BaseService<SysMenuMapper, SysMenu> {
             treeNode.setValue(sysMenu.getId());
             treeNode.setId(sysMenu.getId());
             treeNode.setParentId(sysMenu.getParentId());
-            ofNullable(ids).ifPresent(optIds -> treeNode.setSelected(ArrayUtils.contains(Objects.requireNonNull(StringUtil.getId(optIds)),sysMenu.getId())));
+            ofNullable(ids).ifPresent(optIds -> treeNode.setSelected(ArrayUtils.contains(Objects.requireNonNull(StringUtil.getId(optIds)), sysMenu.getId())));
             return treeNode;
         }).collect(Collectors.toList());
         ListToTree treeBuilder = new ListToTreeImpl();
@@ -135,6 +140,7 @@ public class SysMenuService extends BaseService<SysMenuMapper, SysMenu> {
 
     /**
      * 根据id获取菜单详情
+     *
      * @param id 菜单id
      * @return 菜单详情
      */
@@ -144,6 +150,7 @@ public class SysMenuService extends BaseService<SysMenuMapper, SysMenu> {
 
     /**
      * 获取菜单权限
+     *
      * @return
      */
     public List<String> getMenuPerms(String userName, String roleIds) {
